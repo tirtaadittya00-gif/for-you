2277b21928514e5f25400d012173fabad52d4e58
Humanz <tirtaadittya00@gmail.com>
2026-04-02 12:48:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -252,20 +252,3 @@ document.body.addEventListener("click", () => {
     audioEl.play().catch(() => {});
   }
 }, { once: true });
-
-/* EFEK HATI JATUH (AESTHETIC) */
-setInterval(() => {
-  const heart = document.createElement("div");
-  heart.className = "falling-heart";
-  heart.innerHTML = "❤️";
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = (10 + Math.random() * 20) + "px";
-  heart.style.animationDuration = (4 + Math.random() * 3) + "s";
-
-  document.body.appendChild(heart);
-
-  setTimeout(() => {
-    heart.remove();
-  }, 6000);
-}, 600);
-
